6a81f3a756073c6217eb5f3df7737c733245464e
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2023-12-18 17:09:47 +0200

    feat: add download cv button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components';
+import Link from 'next/link';
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import author from '../../assets/author.png';
@@ -23,6 +25,13 @@ const Header: React.FC = () => {
         <div className={styles.header__inner_textbox}>
           <h1 className={styles.header__title}>Yevhen Rudofylov</h1>
           <h4 className={styles.header__text}>Frontend Developer</h4>
+          <Link
+            className={styles.header__link}
+            target="_blank"
+            href="https://drive.google.com/file/d/1ABvaEYNwM-2YcJgSYJnCfGAgJVt4X-QY/view"
+          >
+            <Button label="Download CV" />
+          </Link>
         </div>
       </div>
     </header>
